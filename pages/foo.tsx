80ef44718499482e2287c6df975985e3affc59e4
Bevan Hunt <bevan@bevanhunt.com>
2022-07-19 11:58:57 -0700

    add foo

---
+++ pages/foo.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+        FOOOOOOOO
+    </div>
+  )
+}
+
+export default Home
